4f8579365193d1ca581c575811936ca49a30a7a7
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2021-06-02 01:05:43 -0300

    Adição de comentários
    espero não ter exagerado que nem na AF9, aquele ficou cabuloso
    
    acho que semana que vem eu vou ver isso e ficar bem preocupado com a minha sanidade (pela quantidade de comments)

--- 1.Procura_de_Palavras/main.py
+++ 1.Procura_de_Palavras/main.py
@@ -1,40 +1,40 @@
-try:
-    arquivo = open('text.txt', 'r')
-except FileNotFoundError:
-    open('text.txt','x')
-    print("Arquivo 'text.txt' criado com sucesso!")
+try: #testa a linha de baixo
+    arquivo = open('text.txt', 'r') #no caso essa linha tenta abrir o arquivo text.txt
+except FileNotFoundError: #ele roda isso se der esse erro especifico
+    open('text.txt','x') #o modo x dentro do open cria um arquivo
+    print("Arquivo 'text.txt' criado com sucesso!") #o modo r dentro do open ali no try é pra ler o arquivo
     input("Pressione enter...")
     exit()
 
-string = []
-palavra = ""
+string = [] #lista string que recebe todas as palavras do arquivo
+frase = "" #serve pra receber a frase antes de entrar dentro da lista uma linha em cima
 print("Este programa atualmente é Case Sensitive, ou seja, letras maíusculas e minúsculas são diferentes!")
-strProcurada = input("Digite uma palavra ou frase: ")
-stringProcurada = []
-hit = 0
-hits = 0
+strProcurada = input("Digite uma palavra ou frase: ") 
+stringProcurada = [] #string procurada
+hit = 0 #serve pra marcar que a letra atual bateu com a letra digitada
+hits = 0 #serve pra contar quantos "matches" foram achados
 
-for l in strProcurada:
-    stringProcurada.append(l)
+for l in strProcurada: #ele pega todos os caracteres dentro da variavel strProcurada, um por um
+    stringProcurada.append(l) #e insere dentro da lista stringProcurada, na ultima posição
 
-for l in arquivo.read():
-    if(l == stringProcurada[hit]):
+for l in arquivo.read(): #isso basicamente trata o arquivo inteiro como uma lista, sendo cada caractere (e break line) como um elemento
+    if(l == stringProcurada[hit]): #se o caractere l é igual ao caractere dentro da lista
         hit += 1
-        if(hit == len(stringProcurada)):
-            print("Frase ou palavra idêntica encontrada na linha:",len(string)+1)
+        if(hit == len(stringProcurada)): #se o total de acertos seguidos equivale ao total de caracteres da frase digitada
+            print("Frase ou palavra idêntica encontrada na linha:",len(string)+1) #ele printa qual linha ele ta agora (len(string))
             hits += 1
             hit = 0
-    else:
+    else: #se não bateu a letra atual com a digitada, o contador de letras iguais é reiniciado
         hit = 0
-    if(l == "\n"):
-        string.append(palavra)
-        palavra = ""
+    if(l == "\n"): #agora se chegou no final da linha de uma frase
+        string.append(frase) #toda a string que tava sendo concatenada ali em baixo é inserida nessa lista
+        frase = "" #e a string é limpada também
     else:
-        palavra += l
+        frase += l #agora se não chegou no final da linha o caractere atual é concatenado na string
 
 if( hits==0 ):
     print("Nenhuma frase ou palavra semelhante foi encontrada...")
 else:
     print("Foram encontrados um total de",hits,"palavra(s) semelhante(s)!")
 input("Pressione enter...")
-exit()
+exit() #espero que esteja bem comentado, se tiver alguma duvida, abra um issue perguntando sobre, só lembre de colocar o link do programa, mais ou menos assim "tenho duvida (nesse)[url vem aqui] programa"
