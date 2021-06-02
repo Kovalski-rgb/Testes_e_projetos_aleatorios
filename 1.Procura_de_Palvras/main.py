ddb4f8657a7bcc11999860ffef0a6ba7f8cf2ae7
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2021-06-01 23:11:31 -0300

    Add files via upload

--- 1.Procura_de_Palvras/main.py
+++ 1.Procura_de_Palvras/main.py
@@ -1,1 +1,33 @@
-.
+arquivo = open('text.txt', 'r')
+
+string = []
+palavra = ""
+print("Este programa atualmente é Case Sensitive, ou seja, letras maíusculas e minúsculas são diferentes!")
+strProcurada = input("Digite uma palavra ou frase: ")
+stringProcurada = []
+hit = 0
+hits = 0
+
+for l in strProcurada:
+    stringProcurada.append(l)
+
+for l in arquivo.read():
+    if(l == stringProcurada[hit]):
+        hit += 1
+        if(hit == len(stringProcurada)):
+            print("Frase ou palavra identica encontrada na linha:",len(string)+1)
+            hits += 1
+            hit = 0
+    else:
+        hit = 0
+    if(l == "\n"):
+        string.append(palavra)
+        palavra = ""
+    else:
+        palavra += l
+
+if( hits==0 ):
+    print("Nenhuma frase ou palavra semelhante foi encontrada...")
+else:
+    print("Foram encontrados um total de",hits,"palavra(s) semelhante(s)!")
+input("Pressione enter...")
